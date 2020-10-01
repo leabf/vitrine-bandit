87df26cb52c4907dd24272e3b55c5efbcebdc2f9
Jean <jean.leoutre@gmail.com>
2020-10-01 15:06:21 +0200

    Connexion bdd+ newsltter

---
+++ bddco.php
@@ -0,0 +1,15 @@
+<?php
+// !! changer nom de la bdd
+
+try{
+$bdd = new PDO('mysql:host=localhost;dbname=projetbandits;charset=utf8', 'root', '');
+$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "INSERT INTO email (libelle_email) VALUES('{$_POST['email']}')";
+$bdd->exec($sql);
+echo "New record";
+}
+catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+$bdd=null;
+?>
